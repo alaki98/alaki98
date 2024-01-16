7275a4d81e2464cc55ec3aeebff19bdfd036d3f3
alaki98 <155637371+alaki98@users.noreply.github.com>
2024-01-16 20:27:04 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,24 +25,6 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
-html_theme_options = {
-    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    # 'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'red',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-html_logo = "final-poster.jpg"
-
 templates_path = ['_templates']
 
 # -- Options for HTML output
