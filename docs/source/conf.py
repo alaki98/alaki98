8b28faf026e50dfbdc5627a07520443f38b34b4a
alaki98 <155637371+alaki98@users.noreply.github.com>
2024-01-16 20:09:55 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'Lumache1111'
 copyright = '2021, Graziella'
 author = 'Graziella'
 
@@ -24,6 +24,24 @@ intersphinx_mapping = {
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
+
+html_theme_options = {
+    # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+html_logo = "final-poster.jpg"
 
 templates_path = ['_templates']
 
